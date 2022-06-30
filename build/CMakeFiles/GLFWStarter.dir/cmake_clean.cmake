1455d0bd3610a2e2691687a31a55f88e0dea7331
Sohaib Alam <sacreative10@gmail.com>
2022-06-30 21:59:17 +0100

    Initial Commit

---
+++ build/CMakeFiles/GLFWStarter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLFWStarter.dir/src/main.cpp.o"
+  "CMakeFiles/GLFWStarter.dir/src/main.cpp.o.d"
+  "CMakeFiles/GLFWStarter.dir/thirdParty/glad/glad.c.o"
+  "CMakeFiles/GLFWStarter.dir/thirdParty/glad/glad.c.o.d"
+  "GLFWStarter"
+  "GLFWStarter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GLFWStarter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
